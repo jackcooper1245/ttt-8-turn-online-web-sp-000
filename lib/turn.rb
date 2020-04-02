f8dbf3f9efd5a55082df09e32e6e49c66225f895
Jack Cooper <jackcooper1245@gmail.com>
2020-04-02 18:16:52 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,6 +29,13 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  
+  #get input
+input_to_index(user_input)
+#convert input to index
+#if index is valid
+#  make the move for input
+#else
+#  ask for input again until you get a valid input
+#end
 end
 
